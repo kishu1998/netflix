44fdcfb9286937867a1c6487b71df955363a4ab7
kishu1998 <kishorsanjaypatil1998@gmail.com>
2019-08-09 19:27:56 +0530

    Updates

---
+++ precache-manifest.3e510b9316c81236c990e4be914539d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cad357eb5b9f67d7cce6cae33c071d",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "7a25c2abe347e5506daf",
+    "url": "/netflix/static/css/main.00d75dd4.chunk.css"
+  },
+  {
+    "revision": "9936583834e4440b0dfe",
+    "url": "/netflix/static/js/2.4c479818.chunk.js"
+  },
+  {
+    "revision": "7a25c2abe347e5506daf",
+    "url": "/netflix/static/js/main.0849e33f.chunk.js"
+  },
+  {
+    "revision": "6a60a5860bbbfa2dbce1",
+    "url": "/netflix/static/js/runtime~main.f479a0f0.js"
+  },
+  {
+    "revision": "ca8632f4d0aacae4633cbfc2f1a673f1",
+    "url": "/netflix/static/media/Anurati.ca8632f4.otf"
+  },
+  {
+    "revision": "715505b3ecb0788be736d33557072559",
+    "url": "/netflix/static/media/logo.715505b3.png"
+  },
+  {
+    "revision": "840718a13c94433a394180dc1574a15f",
+    "url": "/netflix/static/media/logo.840718a1.svg"
+  },
+  {
+    "revision": "bc66b1995aed13b3bf6286224f141ad2",
+    "url": "/netflix/static/media/thunderstrike3d.bc66b199.ttf"
+  }
+]);
